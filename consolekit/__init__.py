925c986f1107694986ee13bb8a9586ba2deac888
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-09 10:26:29 +0000

    Bump version v0.1.2 -> v0.2.0

--- consolekit/__init__.py
+++ consolekit/__init__.py
@@ -42,7 +42,7 @@ from consolekit.input import confirm, prompt
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.2"
+__version__: str = "0.2.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 if not bool(getattr(sys, "ps1", sys.flags.interactive)):
--- doc-source/index.rst
+++ doc-source/index.rst
@@ -83,7 +83,7 @@ consolekit
 	:alt: GitHub top language
 
 .. |commits-since| github-shield::
-	:commits-since: v0.1.2
+	:commits-since: v0.2.0
 	:alt: GitHub commits since tagged version
 
 .. |commits-latest| github-shield::

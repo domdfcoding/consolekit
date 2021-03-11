967ceacf6f5601d361d5bf356916b03866f5ce44
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-11 21:37:51 +0000

    The coincidence issue with "original_datadir" should be fixed now.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,1 @@
-# stdlib
-import os
-from pathlib import Path
-
-# 3rd party
-import pytest
-from domdf_python_tools.paths import PathPlus
-
 pytest_plugins = ("coincidence", "consolekit.testing")
-
-
-@pytest.fixture()
-def original_datadir(request) -> Path:  # noqa: D103
-	# Work around pycharm confusing datadir with test file.
-	return PathPlus(os.path.splitext(request.module.__file__)[0] + '_')

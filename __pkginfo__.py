64f1d128894f48d1b80415f160c7b6b3fdf6cb19
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-06-21 13:31:15 +0100

    Bump version v1.2.2 -> v1.2.3

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -16,7 +16,7 @@ __all__ = [
 		"extras_require",
 		]
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
 extras_require = {
 		"terminals": ["psutil>=5.8.0"],
 		"testing": ["coincidence>=0.1.0", "pytest>=6.0.0", "pytest-regressions>=2.0.2"],
